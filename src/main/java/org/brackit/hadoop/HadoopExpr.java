31d1c81bf756eb4f7ef32b438175753874f65125
Caetano Sauer <csauer@cs.uni-kl.de>
2013-02-19 22:27:15 +0100

    Started implementation of basic Hadoop runtime infrastructure

---
+++ src/main/java/org/brackit/hadoop/HadoopExpr.java
@@ -0,0 +1,162 @@
+/*
+ * [New BSD License]
+ * Copyright (c) 2011-2013, Brackit Project Team <info@brackit.org>  
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Brackit Project Team nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.brackit.hadoop;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.hadoop.conf.Configuration;
+import org.brackit.xquery.ErrorCode;
+import org.brackit.xquery.QueryContext;
+import org.brackit.xquery.QueryException;
+import org.brackit.xquery.Tuple;
+import org.brackit.xquery.compiler.AST;
+import org.brackit.xquery.compiler.XQ;
+import org.brackit.xquery.compiler.XQExt;
+import org.brackit.xquery.module.StaticContext;
+import org.brackit.xquery.util.ExprUtil;
+import org.brackit.xquery.xdm.Expr;
+import org.brackit.xquery.xdm.Item;
+import org.brackit.xquery.xdm.Sequence;
+
+public final class HadoopExpr implements Expr {
+
+	private final Configuration conf;
+	private final StaticContext sctx;
+	private final AST ast;
+	
+	public HadoopExpr(AST ast, StaticContext sctx, Configuration conf)
+	{
+		this.conf = conf;
+		this.sctx = sctx;
+		this.ast = ast;
+	}
+	
+	public HadoopExpr(AST ast, StaticContext sctx)
+	{
+		this(ast, sctx, new Configuration());
+	}
+	
+	public Sequence evaluate(QueryContext ctx, Tuple tuple) throws QueryException
+	{
+		try {
+			ShuffleTree sTree = ShuffleTree.build(ast, null);
+			while (sTree.shuffle != null) {
+				trimJob(sTree, null, ctx, tuple);
+			}
+			return null;
+		}
+		catch (IOException e) {
+			throw new QueryException(e, ErrorCode.BIT_DYN_ABORTED_ERROR);
+		}
+	}
+	
+	private void trimJob(ShuffleTree s, ShuffleTree parent, QueryContext ctx, Tuple tuple)
+			throws IOException, QueryException
+	{
+		while (s.children.size() > 0) {
+			trimJob(s.children.get(0), s, ctx, tuple);
+		}
+		AST root = s.shuffle;
+		while (root.getParent().getType() != XQExt.Shuffle || root.getType() != XQ.End) {
+			root = root.getParent();
+		}
+		root.getParent().deleteChild(root.getChildIndex());
+		if (parent != null) {
+			parent.children.remove(0);
+		}
+		else {
+			s.shuffle = null;
+		}
+		
+		XQueryJobConf jobConf = new XQueryJobConf(conf);
+		jobConf.setAst(root);
+		jobConf.setStaticContext(sctx);
+		if (tuple != null) jobConf.setTuple(tuple);
+		
+		run(jobConf);
+	}
+	
+	private int run(XQueryJobConf jobConf) throws IOException, QueryException
+	{
+		XQueryJob job = new XQueryJob(jobConf);
+		
+		boolean status;
+		try {
+			status = job.waitForCompletion(true);
+			if (!status) {
+				throw new QueryException(ErrorCode.BIT_DYN_ABORTED_ERROR,
+						"Hadoop job execution returned non-zero response");
+			}
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
+		
+		return 0;
+	}
+
+	public Item evaluateToItem(QueryContext ctx, Tuple tuple) throws QueryException
+	{
+		return ExprUtil.asItem(evaluate(ctx, tuple));
+	}
+
+	public boolean isUpdating()
+	{
+		// TODO allow updates
+		return false;
+	}
+
+	public boolean isVacuous()
+	{
+		return false;
+	}
+	
+	private static class ShuffleTree {
+		AST shuffle;
+		ArrayList<ShuffleTree> children = new ArrayList<ShuffleTree>();
+
+		ShuffleTree(AST shuffle, ShuffleTree parent) {
+			this.shuffle = shuffle;
+		}
+		
+		static ShuffleTree build(AST node, ShuffleTree current) {
+			if (node.getType() == XQExt.Shuffle) {
+				ShuffleTree s = new ShuffleTree(node, current);
+				if (current != null) {
+					current.children.add(s);
+				}
+				current = s;
+			}
+			for (int i = 0; i < node.getChildCount(); i++) {
+				build(node.getChild(i), current);
+			}
+			return current;
+		}
+	}
+
+}
---
+++ src/main/java/org/brackit/hadoop/XQueryJobConf.java
@@ -0,0 +1,127 @@
+/*
+ * [New BSD License]
+ * Copyright (c) 2011-2013, Brackit Project Team <info@brackit.org>  
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Brackit Project Team nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.brackit.hadoop;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.hadoop.conf.Configuration;
+import org.brackit.xquery.Tuple;
+import org.brackit.xquery.compiler.AST;
+import org.brackit.xquery.module.StaticContext;
+
+public class XQueryJobConf {
+
+	private Configuration conf;
+	private transient AST ast;
+	
+	public XQueryJobConf(Configuration conf) {
+		this.conf = conf;
+	}
+	
+	public Configuration getConfiguration()
+	{
+		return conf;
+	}
+	
+	public static final String PROP_AST = "org.brackit.hadoop.jobAst";
+	public static final String PROP_SCTX = "org.brackit.hadoop.staticContext";
+	public static final String PROP_TUPLE = "org.brackit.hadoop.contextTuple";
+	
+	public void setAst(AST ast)
+	{
+		this.ast = ast;
+		conf.set(PROP_AST, objectToBase64(ast));
+	}
+	
+	public AST getAst()
+	{
+		if (ast != null) {
+			return ast;
+		}
+		return (AST) base64ToObject(conf.get(PROP_AST));
+	}
+	
+	public void setStaticContext(StaticContext sctx)
+	{
+		conf.set(PROP_SCTX, objectToBase64(sctx));
+	}
+	
+	public StaticContext getStaticContext()
+	{
+		return (StaticContext) base64ToObject(conf.get(PROP_SCTX));
+	}
+
+	public void setTuple(Tuple tuple) {
+		// TODO use tuple serialization
+	}
+	
+	public Tuple getTuple()
+	{
+		String str = conf.get(PROP_TUPLE);
+		if (str == null) {
+			return null;
+		}
+		return null; // TODO use tuple deserialization
+	}
+	
+	public static String objectToBase64(Serializable obj)
+	{		
+		byte[] data;
+		try {
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(bos);
+			oos.writeObject(obj);
+			oos.flush();
+			data = bos.toByteArray();
+		}
+		catch (Exception e) {
+			return null;
+		}
+		
+		return new String(Base64.encodeBase64(data));
+	}
+	
+	public static Object base64ToObject(String b64)
+	{
+		byte[] data = Base64.decodeBase64(b64.getBytes());
+		try {
+			ByteArrayInputStream bis = new ByteArrayInputStream(data);
+			ObjectInputStream ois = new ObjectInputStream(bis);
+			return ois.readObject();
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+	
+}
